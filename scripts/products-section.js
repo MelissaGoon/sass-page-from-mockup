922503ac7fbf462983fe8392cb200e146e6bff02
Melissa Goon <melissagoon13743@gmail.com>
2025-07-31 17:43:59 -0700

    Finished mobile view of splash text and tabs.

--- scripts/products-section.js
+++ scripts/products-section.js
@@ -1,17 +1,17 @@
-const gap = 32;
+const gap = 16;
 const productGallery = document.getElementById("pr-gal-sucres")
 
 const next = document.getElementById("right-arrow-prod");
 const prev = document.getElementById("left-arrow-prod");
 
-let width = productGallery.offsetWidth;
-window.addEventListener("resize", e => (width = productGallery.offsetWidth));
+let width = productGallery.childNodes[1].offsetWidth;
+window.addEventListener("resize", () => (width = productGallery.childNodes[1].offsetWidth));
 
-
-next.addEventListener("click", e => {
+console.log(productGallery.offsetWidth);
+next.addEventListener("click", () => {
     productGallery.scrollBy(width + gap, 0);
 });
-prev.addEventListener("click", e => {
+prev.addEventListener("click", () => {
     productGallery.scrollBy(-(width + gap), 0);
 
 });
