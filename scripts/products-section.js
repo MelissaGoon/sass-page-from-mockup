48c6df2d083c35bb4368e2b9bcf0c4aa1ab93d64
Melissa Goon <melissagoon13743@gmail.com>
2025-07-31 23:42:17 -0700

    Started adding desktop styles, added dynamic sizing for cards and dynamic reading of gap size in script.

--- scripts/products-section.js
+++ scripts/products-section.js
@@ -1,11 +1,16 @@
-const gap = 16;
-const productGallery = document.getElementById("pr-gal-sucres")
+
+const productGallery = document.getElementById("pr-gal-sucres");
+let gap = Number(getComputedStyle(productGallery).gap.slice(0, -2));
 
 const next = document.getElementById("right-arrow-prod");
 const prev = document.getElementById("left-arrow-prod");
 
-let width = productGallery.childNodes[1].offsetWidth;
-window.addEventListener("resize", () => (width = productGallery.childNodes[1].offsetWidth));
+let width = productGallery.offsetWidth;
+window.addEventListener("resize", () => {
+    width = productGallery.offsetWidth
+    gap = Number(getComputedStyle(productGallery).gap.slice(0, -2));
+    console.log(gap)
+});
 
 console.log(productGallery.offsetWidth);
 next.addEventListener("click", () => {
